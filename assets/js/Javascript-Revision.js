57092e11511930296f779c46e4e230f1ebf6fa7b
Ian M <ianmillichamp.2@gmail.com>
2023-01-14 20:59:45 +0000

    define important global variables and connect with html elements

---
+++ assets/js/Javascript-Revision.js
@@ -0,0 +1,10 @@
+/*Add all important variables in this section and define them to correctly order js page */
+let start_btn = document.querySelector(".start_btn button");
+let info_box = document.querySelector(".info_box");
+let quiz_box = document.querySelector(".quiz_box");
+let result_box = document.querySelector(".result_box");
+let option_list = document.querySelector(".option_list");
+let time_line = document.querySelector("header .time_line");
+let timeText = document.querySelector(".timer .time_left_txt");
+let timeCount = document.querySelector(".timer .timer_sec");
+
