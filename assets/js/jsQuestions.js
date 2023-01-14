8dd1a19b3cbbde26b2255cbaaf9c6817d2826769
Ian M <ianmillichamp.2@gmail.com>
2023-01-14 20:52:57 +0000

    added first question to js questions array to test js works will add more questions later

---
+++ assets/js/jsQuestions.js
@@ -0,0 +1,19 @@
+/*create an array consisting of questions to aid in javascript revision.
+Make the array name to be "questions".
+add in a mini array "arrayQuestion"  that gives the question an id number.
+add in a mini array "question" and equal to a string value of the asked question.
+add in a mini array "answers" and equal it to a set of strings containing the question answers.
+add in a mini array "correctAnswer and equal it to the index number of the correct answer , either 0,1,2,3."
+
+*/
+let questions = [{
+    arrayQuestion: 1,
+    question: "What is Javascript?",
+    answers: [
+      "JavaScript is a scripting language used to make the website interactive.",
+      "JavaScript is an assembly language used to make the website interactive",
+      "JavaScript is a compiled language used to make the website interactive",
+      "None of the above"
+    ],
+    correctAnswer: 0
+}]
