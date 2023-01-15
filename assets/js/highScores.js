b5f9f4f411e988aa0012a29dbb8febfded2a42bd
Ian M <ianmillichamp.2@gmail.com>
2023-01-15 20:28:08 +0000

    update high scores js to create function that makes use of local storage

--- assets/js/highScores.js
+++ assets/js/highScores.js
@@ -1,17 +1,20 @@
-function populateHighScores() {
-    // get array from storage, or initialize as empty array
-    let highScoresList = JSON.parse(localStorage.getItem('highScores')) || [];
-    // populate highscores list
+/*Create a function to update the high score list dependant on user entry] */
+
+
+function HighScoresJS() {
+    /*get array from local storage, or initialize as empty array*/
+    let highScoresJSList = JSON.parse(localStorage.getItem('highScoresJS')) || [];
+    /* add details to the  highScores list*/
     let list = '';
-    highScoresList.forEach(score => {
+    highScoresJSList.forEach(score => {
         list = list + '<p>' + score.initials + '  :  ' + score.score + '</p>';
     });
-    highScoresListEl.innerHTML = list;
+    highScoresJSListEl.innerHTML = list;
 }
 
 function resetScores() {
     localStorage.clear();
-    populateHighScores();
+    HighScoresJS();
 }
 
-populateHighScores();
+HighScoresJS();
